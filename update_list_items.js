2e954d278f24fad1f393e690628235ff5c643951
simongaviria1 <simongaviria@ac.c4q.nyc>
2017-10-30 16:39:40 -0400

    fixed bug

--- update_list_items.js
+++ update_list_items.js
@@ -1,17 +1,20 @@
 // With​ ​the​ ​files​ ​update-list-items.html​ ​and​ ​update-list-items.js​,​ ​build​ ​a​ ​page​ ​with​ ​four​ ​list​ ​items, each​ ​with​ ​an​ ​id​ ​attribute​ 
 // ​one​,​ ​two,​ ​ ​three​,​ ​or​ ​four​,​ ​and​ ​each​ ​with​ ​different​ ​text​ ​of​ ​your​ ​choosing. Use​ ​window.prompt()​​ ​to​ ​query​ ​the​ ​user​ ​first​ ​for​ ​an​ ​id​ ​--​ ​
 // one,​ ​two,​ ​three,​ ​or​ ​four​ ​--​ ​and​ ​then prompt​ ​separately​ ​for​ ​a​ ​string.​ ​Use​ ​document.getElementById()​​ ​to​ ​bind​ ​the​ ​list​ ​item specified​ ​in​ ​the​ ​initial​ ​prompt,​ ​
-// and​ ​change​ ​the​ ​text​ ​of​ ​that​ ​list​ ​item​ ​to​ ​the​ ​string​ ​input​ ​from​ ​the second​ ​prompt.​ ​If​ ​the​ ​user's​ ​first​ ​input​ ​is​ ​not​ ​"one",​ ​"two",​ ​"three",​ ​or​ ​"four",​ ​then​ ​simply​ ​change the​ ​text​ ​of​ ​the​ ​
-// first​ ​list​ ​item.
+// and​ ​change​ ​the​ ​text​ ​of​ ​that​ ​list​ ​item​ ​to​ ​the​ ​string​ ​input​ ​from​ ​the second​ ​prompt.​ ​If​ ​the​ ​user's​ ​first​ ​input​ ​is​ ​not​ ​"one",​ ​"two",​ ​"three",​ ​or​ ​"four",​ ​
+// then​ ​simply​ ​change the​ ​text​ ​of​ ​the​ first​ ​list​ ​item.
 
-function replace() {
-    var input = window.prompt("Please pick an id (one, two, three or four)");
+function updateList() {
+    let listItem = window.prompt("Type: one, two, three, four").toLowerCase();
+    let itemText = window.prompt("Add to the list.");
+    let items = ["one", "two", "three", "four"];
 
-
-    var replace = window.prompt("What would you like to replace it with?")
-
-    document.getElementById(input).innerText = replace;
+    if (!items.includes(listItem)) {
+        document.getElementById("one").innerHTML = itemText;
+    } else {
+        document.getElementById(listItem).innerHTML = itemText;
+    }
 
 }
 
-replace();
+updateList();
